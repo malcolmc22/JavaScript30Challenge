b75ff1793f68e0b4082e3f860b9fb70e2b61de36
Malcolm Caleb <malcolmcaleb01@gmail.com>
2024-03-30 21:46:20 -0400

    update and skip function for video player

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,32 @@
+// Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+// Build out functions
+function togglePlay() {
+    // paused is a property that lives on the video
+    // another way of doing pause or play
+    // const method = video.paused ? 'play' : 'pause';
+    // video[method]()
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Hook up event listeners
+video.addEventListener('click', togglePlay)
