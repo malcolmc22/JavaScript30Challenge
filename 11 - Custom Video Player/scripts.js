ef1a49c16b5369910036da09edae257475f4a5bd
Malcolm Caleb <malcolmcaleb01@gmail.com>
2024-03-30 21:49:28 -0400

    event listeners for play puase and timeupdate

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -42,4 +42,7 @@ function scrub(e) {
     video.currentTime = scrubTime
 }
 // Hook up event listeners
-video.addEventListener('click', togglePlay)
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
